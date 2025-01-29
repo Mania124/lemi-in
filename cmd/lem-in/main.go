03286c5d28e5f4701fdf0cffbab77d8d197ebed5
hshikuku <shikukuhezborn@gmail.com>
2025-01-29 10:59:26 +0300

    entry point and outline of tasks

---
+++ cmd/lem-in/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "os"
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+	// Readfile
+	// extract number of ants and room-numbers with connections
+	// find paths then filter most efficient paths
+	// move the ants and display result
+}
