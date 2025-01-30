f1b8fbe84ba63520033132d05e0efc613e378c52
hshikuku <shikukuhezborn@gmail.com>
2025-01-30 11:54:46 +0300

    colony and room structs, filling colony

--- api/colony.go
+++ api/colony.go
@@ -17,15 +17,32 @@ type Room struct {
 
 type Colony struct {
 	nummberOfAnts int
-	rooms         []Room
+	rooms         []*Room
 }
 
-func ColonY(content string) interface{} {
+func NewRoom() *Room {
+	return &Room{
+		start:       false,
+		end:         false,
+		number:      0,
+		connections: nil,
+		visited:     false,
+	}
+}
+
+func NewColony() *Colony {
+	return &Colony{
+		nummberOfAnts: 0,
+		rooms:         nil,
+	}
+}
+
+func ColonY(content string) *Colony {
 	// var count int = 0
-	var colony Colony
-	var room Room
+	colony := NewColony()
+	var room *Room
 	var err error
-	contentSlice := strings.Split(content, "\n")
+	contentSlice := strings.Split(strings.TrimSpace(content), "\n")
 	for i, ch := range contentSlice {
 		if ch[0] == '#' && ch[1] != '#' {
 			continue
@@ -38,6 +55,7 @@ func ColonY(content string) interface{} {
 			}
 		} else {
 			if ch == "##start" {
+				room = NewRoom()
 				room.start = true
 				temp := strings.Fields(strings.TrimSpace(contentSlice[i+1]))
 				room.number, err = strconv.Atoi(temp[0])
@@ -45,9 +63,10 @@ func ColonY(content string) interface{} {
 					log.Fatal(err)
 				}
 				colony.rooms = append(colony.rooms, room)
-				room.start = false
 				contentSlice = append(contentSlice[:i], contentSlice[i+2:]...)
+				fmt.Println(room)
 			} else if ch == "##end" {
+				room = NewRoom()
 				room.end = true
 				temp := strings.Fields(strings.TrimSpace(contentSlice[i+1]))
 				room.number, err = strconv.Atoi(temp[0])
@@ -55,30 +74,33 @@ func ColonY(content string) interface{} {
 					log.Fatal(err)
 				}
 				colony.rooms = append(colony.rooms, room)
+				fmt.Println(room)
 				contentSlice = contentSlice[i+2:]
-				fmt.Println()
-				fmt.Println(strings.Join(contentSlice, "\n"))
-				fmt.Println(contentSlice[0])
+				// fmt.Println()
+				// fmt.Println(strings.Join(contentSlice, "\n"))
+				// fmt.Println(contentSlice)
 				break
 
 			} else {
+				room = NewRoom()
 				temp := strings.Fields(strings.TrimSpace(contentSlice[i]))
 				room.number, err = strconv.Atoi(temp[0])
 				if err != nil {
 					log.Fatal(err)
 				}
 				colony.rooms = append(colony.rooms, room)
+				fmt.Println(room)
 			}
 		}
 
 	}
 	for _, word := range contentSlice {
 		temp := Split(strings.TrimSpace(word))
-		fmt.Println(temp)
+		// fmt.Println(len(temp), "size")
 		// break
 		for _, w := range colony.rooms {
 			rmn, _ := strconv.Atoi(temp[0])
-			fmt.Println(rmn)
+			// fmt.Println(rmn)
 			conn, _ := strconv.Atoi(temp[1])
 			if w.number == rmn {
 				w.connections = append(w.connections, conn)
